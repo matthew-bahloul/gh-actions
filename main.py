90df0a6900d4efc3f959baa550618d47e090b424
Matthew Bahloul <matthew.bahloul@gmail.com>
2023-09-06 23:19:54 -0700

    updates

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromiumService
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
 
 
 if __name__ == '__main__':
-    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get('https://google.com')
     print('peepee poopoo')
